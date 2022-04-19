6017bbb8f595724e64ef75e0665662f27d8c1bff
vyphotphet100 <caovy2001@gmail.com>
2022-04-19 14:07:26 +0700

    done bottom nav

--- app/src/main/java/hcmute/edu/vn/caodinhsyvy_19110143/foody/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/caodinhsyvy_19110143/foody/MainActivity.java
@@ -1,39 +1,62 @@
 package hcmute.edu.vn.caodinhsyvy_19110143.foody;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
 
     View homeActivityView, ordersActivityView, accountActivityView;
-    ImageView btnHome;
+    RelativeLayout placeHolder;
+//    ImageView btnHome;
     HomeActivity homeActivityClass;
+    FloatingActionButton btnHome;
+    CurvedBottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RelativeLayout placeHolder = findViewById(R.id.mainLayout);
+        placeHolder = findViewById(R.id.mainLayout);
         homeActivityView = (new HomeActivity(MainActivity.this)).getView();
         ordersActivityView = View.inflate(this, R.layout.activity_orders, null);
         accountActivityView = View.inflate(this, R.layout.activity_account, null);
-        btnHome = findViewById(R.id.imgHome);
+        btnHome = findViewById(R.id.btnHome);
+        bottomNav = findViewById(R.id.bottomNav);
 
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                bottomNav.setSelectedItemId(R.id.action_home);
+            }
+        });
+
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 placeHolder.removeView(homeActivityView);
                 placeHolder.removeView(ordersActivityView);
                 placeHolder.removeView(accountActivityView);
-                placeHolder.addView(homeActivityView);
-                if (homeActivityClass == null)
-                    homeActivityClass = new HomeActivity(MainActivity.this);
+
+                if (item.getItemId() == R.id.action_home) {
+                    GoToHome();
+                } else if (item.getItemId() == R.id.action_orders) {
+
+                } else if (item.getItemId() == R.id.action_account) {
+
+                }
+
+                return true;
             }
         });
 
@@ -60,5 +83,11 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 //        bottomNav.setSelectedItemId(R.id.action_home);
+    }
+
+    private void GoToHome() {
+        placeHolder.addView(homeActivityView);
+        if (homeActivityClass == null)
+            homeActivityClass = new HomeActivity(MainActivity.this);
     }
 }
---
+++ app/src/main/java/hcmute/edu/vn/caodinhsyvy_19110143/foody/OrdersActivity.java
@@ -0,0 +1,4 @@
+package hcmute.edu.vn.caodinhsyvy_19110143.foody;
+
+public class OrdersActivity {
+}
